6d70d6cb1199260b4fbd34d58d55f0c302e83398
hubert-lasota <hxprogramming@gmail.com>
2024-06-16 16:37:27 +0200

    wstępny commit

---
+++ src/main/java/org/hl/wirtualnyregalbackend/WirtualnyRegalBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hl.wirtualnyregalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirtualnyRegalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WirtualnyRegalBackendApplication.class, args);
+    }
+
+}
